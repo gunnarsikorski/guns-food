082b485ad97018217076af4b02443fe8d2d8433e
gunnarsikorski <gunnarsikorski@gmail.com>
2021-08-03 16:22:30 -0500

    Added summary on home and dummy meal list

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import classes from './AvailableMeals.Module.css'
+
+const DUMMY_MEALS = [
+	{
+		id: 1,
+		name: 'Sushi',
+		description: 'Finest fish and veggies',
+		price: 22.99,
+	},
+	{
+		id: 2,
+		name: 'Schnitzel',
+		description: 'A german specialty!',
+		price: 16.5,
+	},
+	{
+		id: 3,
+		name: 'Barbecue Burger',
+		description: 'American, raw, meaty',
+		price: 12.99,
+	},
+	{
+		id: 4,
+		name: 'Green Bowl',
+		description: 'Healthy...and green...',
+		price: 18.99,
+	},
+];
+
+const AvailableMeals = () => {
+	const mealList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+
+	return (
+		<>
+			<section className={classes.meals}>
+				<ul>{mealList}</ul>
+			</section>
+		</>
+	);
+};
+
+export default AvailableMeals;
