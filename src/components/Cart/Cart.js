21f2b66031e1a17c61f706a9908ffc39fbc61e0a
gunnarsikorski <gunnarsikorski@gmail.com>
2021-08-09 14:51:43 -0500

    Added open and close ability on modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,15 +12,17 @@ const Cart = (props) => {
 	);
 
 	return (
-		<Modal>
+		<Modal onClose={props.onClose}>
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
 				<span>50 bucks</span>
 			</div>
 			<div className={classes.actions}>
-				<button className={classes['button--alt']}>Close</button>
 				<button className={classes.button}>Order</button>
+				<button className={classes['button--alt']} onClick={props.onClose}>
+					Close
+				</button>
 			</div>
 		</Modal>
 	);
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,11 +5,14 @@ import chickenImg from '../../assets/chicken.jpeg'
 import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
+
+
+
     return (
         <>
            <header className={classes.header}>
                <h1>Gun's Restaurant</h1>
-               <HeaderCartButton />
+               <HeaderCartButton onClick={props.onShowCart}/>
            </header>
            <div className={classes['main-image']}>
                <img src={chickenImg} alt='Fried chicken wings'/>
