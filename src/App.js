a1bd61f4fcc27bb4775bed4371c16ff47049820f
gunnarsikorski <gunnarsikorski@gmail.com>
2021-08-10 15:15:25 -0500

    Added context for cart dummy data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useState } from 'react'
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider';
 
 function App() {
 
@@ -17,13 +19,13 @@ function App() {
 	}
 
 	return (
-		<>
+		<CartProvider>
 			{cartIsShown && <Cart onClose={hideCartHandler}/>}
 			<Header onShowCart={showCartHandler}/>
 			<main>
 				<Meals />
 			</main>
-		</>
+		</CartProvider>
 	);
 }
 
