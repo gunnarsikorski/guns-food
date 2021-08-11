c4ac6f2a67c09bf52bc0c884d8590f22dd2c9e90
gunnarsikorski <gunnarsikorski@gmail.com>
2021-08-11 12:20:48 -0500

    Getting total amount to be dynamic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
+
+    const cartCtx = useContext(CartContext)
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+    const hasItems = cartCtx.items.length > 0
+
 	const cartItems = (
 		<ul className={classes['cart-items']}>
-			{[{ id: 1, name: 'Sushi', amount: 2, price: 11.99 }].map((item) => (
+			{cartCtx.items.map((item) => (
 				<li>{item.name}</li>
 			))}
 		</ul>
@@ -16,10 +23,10 @@ const Cart = (props) => {
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
-				<span>50 bucks</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={classes.actions}>
-				<button className={classes.button}>Order</button>
+				{hasItems && <button className={classes.button}>Order</button>}
 				<button className={classes['button--alt']} onClick={props.onClose}>
 					Close
 				</button>
