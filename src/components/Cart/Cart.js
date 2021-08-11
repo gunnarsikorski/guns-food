d1441f319f19bc7432f8c30734da718c2bc71cea
gunnarsikorski <gunnarsikorski@gmail.com>
2021-08-11 13:37:33 -0500

    Making cart items look better and separate

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
+	const cartCtx = useContext(CartContext);
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
 
-    const cartCtx = useContext(CartContext)
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
-    const hasItems = cartCtx.items.length > 0
+	const cartItemRemoveHandler = (id) => {
+
+    };
+
+	const cartItemAddHandler = (item) => {
+
+    };
 
 	const cartItems = (
 		<ul className={classes['cart-items']}>
 			{cartCtx.items.map((item) => (
-				<li>{item.name}</li>
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	);
