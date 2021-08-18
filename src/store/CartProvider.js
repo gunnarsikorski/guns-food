0d9401ffa03637255c9dc8474690e3c9197613eb
gunnarsikorski <gunnarsikorski@gmail.com>
2021-08-18 14:58:16 -0500

    Enabled buttons to add and remove food items dynamically

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,11 +11,11 @@ const cartReducer = (state, action) => {
 		const updatedTotalAmount =
 			state.totalAmount + action.item.price * action.item.amount;
 
-		const existingcartItemIndex = state.item.findIndex(
+		const existingCartItemIndex = state.item.findIndex(
 			(item) => item.id === action.item.id
 		);
 
-		const existingCartItem = state.items[existingcartItemIndex];
+		const existingCartItem = state.items[existingCartItemIndex];
 		let updatedItems;
 
 		if (existingCartItem) {
@@ -24,16 +24,40 @@ const cartReducer = (state, action) => {
 				amount: existingCartItem.amount + action.item.amount,
 			};
 			updatedItems = [...state.items];
-			updatedItems[existingcartItemIndex] = updatedItem;
+			updatedItems[existingCartItemIndex] = updatedItem;
 		} else {
 			updatedItems = state.items.concat(action.item)
 			// better than push b/c brings back new array
 		}
+
+		
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
-		};
+		}
+
 	}
+
+	if (action.type === 'REMOVE_ITEM') {
+		const existingCartItemIndex = state.item.findIndex(
+			(item) => item.id === action.id
+			);
+			const existingItem = state.items[existingCartItemIndex]
+			const updatedTotalAmount = state.totalAmount - existingItem.price
+			let updatedItems
+			if (existingItem.amount === 1) {
+				updatedItems = state.items.filter(item => item.id !== action.id)
+			} else {
+				const updatedItem = { ...existingItem, amount: existingItem.amount - 1}
+				updatedItems = [...state.items]
+				updatedItems[existingCartItemIndex] = updatedItem
+			}
+			return {
+				items: updatedItems,
+				totalAmount: updatedTotalAmount
+			}
+	}
+
 	return defaultCartState;
 };
 
